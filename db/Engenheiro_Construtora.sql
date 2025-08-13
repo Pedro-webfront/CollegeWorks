e16f5085e41fe6a84247da7ae9f1e9674ca5f595
Pedro Faculdade <pedro.coelho7@estudante.ifto.edu.br>
2025-08-12 21:40:32 -0300

    adiciona as aulas de bd do dia 12/08

---
+++ db/Engenheiro_Construtora.sql
@@ -0,0 +1,5 @@
+INSERT INTO `Engenheiro`(`codigo`, `nome`) VALUES (1,'Pedro')
+INSERT INTO `Engenheiro`(`codigo`, `nome`) VALUES (2,'Henrique')
+
+
+INSERT INTO `Engenheiro`(`codigo`, `nome`) VALUES ('[value-1]','[value-2]')
